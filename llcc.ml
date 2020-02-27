1bcf5fe04396d7898d874b3971fe976dafc81db4
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2020-02-28 01:45:58 +0900

    Add parser

--- llcc.ml
+++ llcc.ml
@@ -1,15 +1,15 @@
 open Batteries
 
-let (let*) m k =
-  match m with
-  | Ok x -> k x
-  | Bad _  as e ->  e
+module Result = struct
+  include Result
 
-let return x = Ok x
+  let (let*) m k =
+    match m with
+    | Ok x -> k x
+    | Error _  as e ->  e
 
-let apply f g = function
-  | Ok o -> f o
-  | Bad b -> g b
+  let return x = Ok x
+end
 
 type pos = int
 
@@ -17,10 +17,14 @@ type op =
   | Plus
   | Minus
 
+let print_op = function
+  | Plus -> "+"
+  | Minus -> "-"
+
 let op_of_char = function
   | '+'-> Ok Plus
   | '-'-> Ok Minus
-  | _ -> Bad "invalid token"
+  | _ -> Error "invalid token"
 
 type token =
   | Reserved of pos * op
@@ -52,70 +56,81 @@ let tokenize input =
         Result.map (List.cons (Num (i, n))) @@ aux i r
       | ' ' | '\t' -> aux (succ i) rest
       | '+' | '-' ->
-        let* op = op_of_char c in
-        Result.map (List.cons (Reserved (i, op))) @@ aux (succ i) rest
-      | _  -> Bad (error_message "tokenize" i "unexpected token")
+        Result.(let* op = op_of_char c in
+                map (List.cons (Reserved (i, op))) @@ aux (succ i) rest)
+      | _  -> Error (error_message "tokenize" i "unexpected token")
   in
   aux 0 @@ String.to_list input
 
 type node =
-  | BinaryOp of pos * op * node * node
   | Number of pos * int
+  | BinaryOp of pos * op * node * node
+
+let rec print_node = function
+  | Number(_, n)  -> Printf.sprintf "Number(%d)" n
+  | BinaryOp(_, op, l, r) ->
+    let sl = print_node l in
+    let sr = print_node r in
+    Printf.sprintf "BinaryOp(%s,%s,%s)\n" (print_op op) sl sr
 
 let primary =
-  let open State in
-  let+ tokens = get in
-  match tokens with
-  | [] -> return (Bad "token exhausted")
-  | t :: rest->
-    let+ () = put rest in
-    match t with
-    | Num (i, n) -> return (Ok (Number (i, n)))
-    | _ -> return (Bad "unexpected token")
+  State.(let+ tokens = get in
+         match tokens with
+         | [] -> return (Error "token exhausted")
+         | t :: rest->
+           let+ () = put rest in
+           match t with
+           | Num (i, n) -> return (Ok (Number (i, n)))
+           | _ -> return (Error "unexpected token"))
 
 let mul = primary
 
+(* expr  = mul ("+" mul | "-" mul)* *)
 let expr =
-  let open State in
   let rec star left =
-    let+ tokens = get in
-    match tokens with
-    | [] -> State.return left
-    | t::rest ->
-      let+ () = put rest in
-      match t with
-      | Reserved (i, op) ->
-        let+ right = mul in
-        let* lnode = left in
-        let* rnode = right in
-        State.return (BinaryOp (i, op, lnode, rnode))
-      | _ -> State.return left in
-  let+ r = mul in
-  let* n = r in
-  star n
+    State.(let+ tokens = get in
+           match tokens with
+           | [] -> return left
+           | t::rest ->
+             let+ () = put rest in
+             match t with
+             | Reserved (i, op) ->
+               let+ right = mul in
+               let n = Result.(let* lnode = left in
+                               let* rnode = right in
+                               return @@ BinaryOp (i, op, lnode, rnode)) in
+               star n
+             | _ -> return left) in
+  State.(let+ left = mul in
+         star left)
 
-let parse = expr
+(* program = expr *)
+let program = expr
+
+let parse = program
 
 let rec generate  = function
   | Number (_, n) -> Ok ([Printf.sprintf "\tpush %d" n])
   | BinaryOp (_, op, left, right) ->
-    let* lcom = generate left in
-    let* rcom = generate right in
-    match op with
-    | Plus -> return @@ lcom  @ ["\tpop rdi"; "\tpop rax"; "\tadd rax, rdi"; "\tpush rax"] @ rcom
-    | Minus -> return @@ lcom @ ["\tpop rdi"; "\tpop rax"; "\tsub rax, rdi"; "\tpush rax"] @ rcom
+    Result.(let* lcom = generate left in
+            let* rcom = generate right in
+            match op with
+            | Plus -> return @@ lcom  @ rcom @  ["\tpop rdi"; "\tpop rax"; "\tadd rax, rdi"; "\tpush rax"]
+            | Minus -> return @@ lcom @ rcom @ ["\tpop rdi"; "\tpop rax"; "\tsub rax, rdi"; "\tpush rax"])
 
-                                                                                             (* * let () =
-                                                                                              * *   (if Array.length Sys.argv != 2 then
-                                                                                              * *      Bad "引数の個数が正しくありません"
-                                                                                              * *    else
-                                                                                              * *      begin
-                                                                                              * *        input := Sys.argv.(1);
-                                                                                              * *        let* tokens = tokenize !input in
-                                                                                              * *        let* (parsed, _) = parse tokens in
-                                                                                              * *        let* commands = generate parsed in
-                                                                                              * *        return @@ String.concat "\n" @@
-                                                                                              * *        [".intel_syntax noprefix";
-                                                                                              * *         ".global main"; "main:"] @ commands @ ["\tpop rax"; "\tret"]
-                                                                                              * *      end)
-                                                                                              * *   |> apply (fun s -> Printf.printf "%s\n" s; exit 0) (fun s -> Printf.eprintf "%s\n" s ; exit 1) *) *)
+let () =
+  (if Array.length Sys.argv != 2 then
+     Error "引数の個数が正しくありません"
+   else
+     begin
+       input := Sys.argv.(1);
+       Result.(let* tokens = tokenize !input in
+               let (parsed, _) = State.runState parse tokens in
+               let* p = parsed in
+               let* commands = generate p in
+               return @@ String.concat "\n" @@
+               [".intel_syntax noprefix";
+                ".global main"; "main:"] @ commands @ ["\tpop rax"; "\tret"])
+     end)
+  |> Result.fold ~ok:(fun s -> Printf.printf "%s\n" s;0) ~error:(fun s -> Printf.eprintf "%s\n" s ;1)
+  |> exit
