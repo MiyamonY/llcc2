d2d75333f7cd7a2c579daee3dcff83a6e156a7e7
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2020-03-07 19:45:31 +0900

    Refactor

--- generator.ml
+++ generator.ml
@@ -39,37 +39,40 @@ let generate_lval = function
 
 let ret = [Machine "pop rax"; Machine "mov rsp, rbp"; Machine "pop rbp"; Machine "ret"]
 
+let machine_of_op op i =
+  let open Operator in
+  Result.(match op with
+      | Plus -> return [Machine "add rax, rdi"]
+      | Minus -> return [Machine "sub rax, rdi"]
+      | Mul -> return [Machine "imul rax, rdi"]
+      | Div -> return [Machine "cqo";
+                       Machine "idiv rdi";]
+      | Eq -> return [Machine "cmp rax, rdi";
+                      Machine "sete al";
+                      Machine "movzb rax, al";]
+      | Neq -> return [Machine "cmp rax, rdi";
+                       Machine "setne al";
+                       Machine "movzb rax, al";]
+      | Lt -> return [Machine "cmp rax, rdi";
+                      Machine "setl al";
+                      Machine "movzb rax, al";]
+      | Le -> return [Machine "cmp rax, rdi";
+                      Machine "setle al";
+                      Machine "movzb rax, al";]
+      | Assign -> return [Machine "mov [rax], rdi"; Machine "push rdi"]
+      | _ -> error @@ `GeneratorError (Some i, "invalid operator"))
+
 let rec generate_node = function
   | Number (_, n) ->
     Result.(return [Machine (Printf.sprintf "push %d" n)])
   | BinaryOp (i, op, left, right) ->
-    Result.(
-      let* lcom =
-        if op = Assign then generate_lval left
-        else generate_node left in
-      let* rcom = generate_node right in
-      let* com = match op with
-        | Plus -> return [Machine "add rax, rdi"]
-        | Minus -> return [Machine "sub rax, rdi"]
-        | Mul -> return [Machine "imul rax, rdi"]
-        | Div -> return [Machine "cqo";
-                         Machine "idiv rdi";]
-        | Eq -> return [Machine "cmp rax, rdi";
-                        Machine "sete al";
-                        Machine "movzb rax, al";]
-        | Neq -> return [Machine "cmp rax, rdi";
-                         Machine "setne al";
-                         Machine "movzb rax, al";]
-        | Lt -> return [Machine "cmp rax, rdi";
-                        Machine "setl al";
-                        Machine "movzb rax, al";]
-        | Le -> return [Machine "cmp rax, rdi";
-                        Machine "setle al";
-                        Machine "movzb rax, al";]
-        | Assign -> return [Machine "mov [rax], rdi"; Machine "push rdi"]
-        | _ -> error @@ `GeneratorError (Some i, "invalid operator") in
-      return @@ lcom @ rcom @ [Machine "pop rdi"; Machine "pop rax";] @ com @
-                if op = Assign then [] else [Machine "push rax"])
+    Result.(let* lcom =
+              if op = Assign then generate_lval left
+              else generate_node left in
+            let* rcom = generate_node right in
+            let* com = machine_of_op op i in
+            return @@ lcom @ rcom @ [Machine "pop rdi"; Machine "pop rax";] @ com @
+                      if op = Assign then [] else [Machine "push rax"])
   | Variable(_, _) as n ->
     Result.(let* lcom = generate_lval n in
             return @@ lcom @ [Machine "pop rax"; Machine "mov rax, [rax]"; Machine "push rax"])
