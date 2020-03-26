4a9d6828002860956807359e3720e9c5c7eccdf8
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2020-03-26 22:22:02 +0900

    Refactor

--- generator.ml
+++ generator.ml
@@ -23,60 +23,67 @@ module Label = struct
     label
 end
 
+let (let>) = Result.Monad.bind
+let tell = Result.ok % Writer.tell
+let error = Result.error
+let seq ls =
+  List.fold_left (fun pred next ->
+      let> p = pred in
+      let> n = next in
+      Result.Monad.return (p >>> n)) (tell []) ls
+
 let machine_of_op op i =
   let open Operator in
   match op with
-  | Plus -> Writer.tell [Machine "add rax, rdi"]
-  | Minus -> Writer.tell [Machine "sub rax, rdi"]
-  | Mul -> Writer.tell [Machine "imul rax, rdi"]
-  | Div -> Writer.tell [Machine "cqo";
-                        Machine "idiv rdi";]
-  | Eq -> Writer.tell [Machine "cmp rax, rdi";
-                       Machine "sete al";
-                       Machine "movzb rax, al";]
-  | Neq -> Writer.tell [Machine "cmp rax, rdi";
-                        Machine "setne al";
-                        Machine "movzb rax, al";]
-  | Lt -> Writer.tell [Machine "cmp rax, rdi";
-                       Machine "setl al";
-                       Machine "movzb rax, al";]
-  | Le -> Writer.tell [Machine "cmp rax, rdi";
-                       Machine "setle al";
-                       Machine "movzb rax, al";]
-  | Assign -> Writer.tell [Machine "mov [rax], rdi";
-                           Machine "push rdi"]
-  | _ -> Writer.return @@ Result.error @@ `GeneratorError (Some i, "invalid operator")
+  | Plus -> tell [Machine "add rax, rdi"]
+  | Minus -> tell [Machine "sub rax, rdi"]
+  | Mul -> tell [Machine "imul rax, rdi"]
+  | Div -> tell [Machine "cqo";
+                 Machine "idiv rdi";]
+  | Eq -> tell [Machine "cmp rax, rdi";
+                Machine "sete al";
+                Machine "movzb rax, al";]
+  | Neq -> tell [Machine "cmp rax, rdi";
+                 Machine "setne al";
+                 Machine "movzb rax, al";]
+  | Lt -> tell [Machine "cmp rax, rdi";
+                Machine "setl al";
+                Machine "movzb rax, al";]
+  | Le -> tell [Machine "cmp rax, rdi";
+                Machine "setle al";
+                Machine "movzb rax, al";]
+  | Assign -> tell [Machine "mov [rax], rdi";
+                    Machine "push rdi"]
+  | _ -> error @@ `GeneratorError (Some i, "invalid operator")
 
 let generate_lval local = function
   | Variable(i, name) ->
     begin match Local.find local name with
       | None ->
-        Writer.return @@
-        Result.error @@ `GeneratorError (Some i, Printf.sprintf "variable %s not found" name)
+        error @@ `GeneratorError (Some i, Printf.sprintf "variable %s not found" name)
       | Some n ->
-        Writer.tell [Machine "mov rax, rbp";
-                     Machine (Printf.sprintf "sub rax, %d" @@ n);
-                     Machine "push rax"]
+        tell [Machine "mov rax, rbp";
+              Machine (Printf.sprintf "sub rax, %d" @@ n);
+              Machine "push rax"]
     end
   | _ as n ->
-    Writer.return @@
-    Result.error @@ `GeneratorError (Some (at n), Printf.sprintf "%s is not left value" @@ Parser.to_string n)
+    error @@ `GeneratorError (Some (at n), Printf.sprintf "%s is not left value" @@ Parser.to_string n)
 
 let init =
-  Writer.tell [Assembler ".intel_syntax noprefix";
-               Assembler ".global main";]
+  tell [Assembler ".intel_syntax noprefix";
+        Assembler ".global main";]
 
-let prolog =
-  Writer.tell [Machine "push rbp";
-               Machine "mov rbp, rsp";]
+let prologue =
+  tell [Machine "push rbp";
+        Machine "mov rbp, rsp";]
 
-let label name = Writer.tell [Label name]
+let label name = tell [Label name]
 
 let epiloge =
-  Writer.tell [Machine "pop rax";
-               Machine "mov rsp, rbp";
-               Machine "pop rbp";
-               Machine "ret"]
+  tell [Machine "pop rax";
+        Machine "mov rsp, rbp";
+        Machine "pop rbp";
+        Machine "ret"]
 
 let move_arguments_to_stack local args =
   let regs = ["rdi"; "rsi"; "rdx"; "rcx"; "r8"; "r9";] in
@@ -84,164 +91,125 @@ let move_arguments_to_stack local args =
   List.fold_left (fun pred (name, reg) ->
       match Local.find local name with
       | None ->
-        Writer.return @@
-        Result.error @@ `GeneratorError (None, Printf.sprintf "%s not found in local" @@ name)
+        error @@ `GeneratorError (None, Printf.sprintf "%s not found in local" @@ name)
       | Some n ->
-        pred >>>
-        Writer.tell [Machine "mov rax, rbp";
-                     Machine (Printf.sprintf "sub rax, %d" @@ n);
-                     Machine "push rax";
-                     Machine "pop rax";
-                     Machine (Printf.sprintf "mov [rax], %s" reg)])
-    (Writer.return (Ok ())) maps
+        seq [pred;
+             tell [Machine "mov rax, rbp";
+                   Machine (Printf.sprintf "sub rax, %d" @@ n);
+                   Machine "push rax";
+                   Machine "pop rax";
+                   Machine (Printf.sprintf "mov [rax], %s" reg)]])
+    (tell []) maps
 
 let assign_local_variable n =
-  Writer.tell [Machine (Printf.sprintf "sub rsp, %d" n)]
+  tell [Machine (Printf.sprintf "sub rsp, %d" n)]
 
 let rec generate_node local = function
-  | Number (_, n) -> Writer.tell [Machine (Printf.sprintf "push %d" n)]
+  | Number (_, n) -> tell [Machine (Printf.sprintf "push %d" n)]
   | BinaryOp (i, op, left, right) ->
-    let@ result = (if op = Assign then generate_lval local else generate_node local) left in
-    begin match result with
-      | Error _ as err -> Writer.return err
-      | Ok _ ->
-        let@ result = generate_node local right in
-        begin match result with
-          | Error _ as err -> Writer.return err
-          | Ok _ ->
-            Writer.tell [Machine "pop rdi"; Machine "pop rax";] >>>
-            let@ result = machine_of_op op i in
-            match result with
-            | Error _ as err -> Writer.return err
-            | Ok _ -> Writer.tell (if op = Assign then [] else [Machine "push rax"])
-        end
-    end
+    let left =
+      (if op = Assign then generate_lval else generate_node) local left in
+    seq [left;
+         generate_node local right;
+         tell [Machine "pop rdi"; Machine "pop rax";];
+         machine_of_op op i;
+         tell (if op = Assign then [] else [Machine "push rax"])]
+  | FuncDecl (_, name, local, args, body) ->
+    seq [label name;
+         prologue;
+         move_arguments_to_stack local args;
+         assign_local_variable @@ Local.assign_size local;
+         generate_nodes local body;
+         epiloge]
+  | Return(_, node) ->
+    seq [generate_node local node;
+         epiloge]
   | If(_, cond, then_, else_) ->
     let lelse = Label.create () in
     let lend = Label.create () in
-    let@ _ = generate_node local cond in
-    Writer.tell [Machine "pop rax";
-                 Machine "cmp rax, 0";
-                 Machine (Printf.sprintf "je %s" lelse)] >>>
-    let@ result = generate_node local then_ in
-    begin match result with
-      | Error _ as err -> Writer.return err
-      | Ok _ ->
-        Writer.tell [Machine (Printf.sprintf "jmp %s" lend)] >>>
-        label lelse >>>
-        match else_ with
-        | None -> label lend
-        | Some node ->
-          let@ result = generate_node local node in
-          match result with
-          | Error _ as err -> Writer.return err
-          | Ok _ -> label lend
-    end
-  | Variable(_, _) as n ->
-    let@ result = generate_lval local n in
-    begin match result with
-      | Error _ as err -> Writer.return err
-      | Ok _ ->
-        Writer.tell [Machine "pop rax"; Machine "mov rax, [rax]"; Machine "push rax"]
-    end
-  | Return(_, node) ->
-    begin let@ result = generate_node local node in
-      match result with
-      | Error _ as err -> Writer.return err
-      | Ok _ -> epiloge
-    end
+    let else_ = match else_ with
+      | None -> [label lend]
+      | Some node ->
+        [generate_node local node;
+         label lend] in
+    seq ([generate_node local cond;
+          tell [Machine "pop rax";
+                Machine "cmp rax, 0";
+                Machine (Printf.sprintf "je %s" lelse)];
+          generate_node local then_;
+          tell [Machine (Printf.sprintf "jmp %s" lend)];
+          label lelse;
+         ] @ else_)
   | While(_, cond, body) ->
     let lbegin = Label.create ~label:"WhileBegin" () in
     let lend = Label.create ~label:"WhileEnd" () in
-    label lbegin >>>
-    let@ result = generate_node local cond in
-    begin match result with
-      | Error _ as err -> Writer.return err
-      | Ok _ ->
-        Writer.tell [Machine "pop rax";
-                     Machine "cmp rax, 0";
-                     Machine (Printf.sprintf "je %s" lend);] >>>
-        let@ result = generate_node local body in
-        match result with
-        | Error _ as err -> Writer.return err
-        | Ok _ ->
-          Writer.tell [Machine (Printf.sprintf "jmp %s" lbegin)]>>>
-          label lend
-    end
+    seq [label lbegin;
+         generate_node local cond;
+         tell [Machine "pop rax";
+               Machine "cmp rax, 0";
+               Machine (Printf.sprintf "je %s" lend);];
+         generate_node local body;
+         tell [Machine (Printf.sprintf "jmp %s" lbegin)];
+         label lend]
+  | Variable(_, _) as n ->
+    seq [generate_lval local n;
+         tell [Machine "pop rax"; Machine "mov rax, [rax]";
+               Machine "push rax"]]
   | For(_, init, cond, next, body) ->
-    begin
-      let lrep = Label.create ~label:"ForRep" () in
-      let lend = Label.create ~label:"ForEnd" () in
-      let@ result = match init with
-        | None -> Writer.tell []
-        | Some node -> generate_node local node in
-      match result with
-      | Error _ as err -> Writer.return err
-      | Ok _ ->
-        label lrep >>>
-        let@ result = generate_node local body in
-        match result with
-        | Error _ as err -> Writer.return err
-        | Ok () ->
-          let@ result = match cond with
-            | None -> Writer.tell []
-            | Some node -> generate_node local node in
-          match result with
-          | Error _ as err -> Writer.return err
-          | Ok _ ->
-            Writer.tell [Machine "pop rax";
-                         Machine "cmp rax, 0";
-                         Machine (Printf.sprintf "je %s" lend);] >>>
-            match next with
-            | None ->
-              Writer.tell [Machine (Printf.sprintf "jmp %s" lrep)] >>>
-              label lend
-            | Some node ->
-              let@ result = generate_node local node in
-              match result with
-              | Error _ as err -> Writer.return err
-              | Ok _ ->
-                Writer.tell [Machine (Printf.sprintf "jmp %s" lrep)]>>>
-                label lend
-    end
+    let lrep = Label.create ~label:"ForRep" () in
+    let lend = Label.create ~label:"ForEnd" () in
+    let init = match init with
+      | None -> tell []
+      | Some node -> generate_node local node in
+    let cond = match cond with
+      | None -> tell []
+      | Some node -> generate_node local node in
+    let next = match next with
+      | None ->
+        [tell [Machine (Printf.sprintf "jmp %s" lrep)]]
+      | Some node ->
+        [generate_node local node;
+         tell [Machine (Printf.sprintf "jmp %s" lrep)]] in
+    seq ([init;
+          label lrep;
+          generate_node local body;
+          cond;
+          tell [Machine "pop rax";
+                Machine "cmp rax, 0";
+                Machine (Printf.sprintf "je %s" lend);]]
+         @ next
+         @ [label lend])
   | Block(_, nodes) ->
     List.fold_left
-      (fun pred node -> pred >>> generate_node local node >>> Writer.tell [Machine "pop rax"])
-      (Writer.return (Ok ())) nodes
+      (fun pred node ->
+         seq [pred;
+              generate_node local node;
+              tell [Machine "pop rax"];])
+      (tell []) nodes
   | FuncCall(_, name, args)->
     let arguments args =
       let regs = ["rdi"; "rsi"; "rdx"; "rcx"; "r8"; "r9";] in
       let maps = List.combine args @@ List.take (List.length args) regs in
-      List.fold_left (fun pred (node, reg) -> pred >>>
-                       let@ _ = generate_node local node in
-                       Writer.tell [Machine "pop rax"] >>>
-                       Writer.tell [Machine (Printf.sprintf "mov %s, rax" reg)])
-
-        (Writer.return (Ok ())) @@ List.rev maps in
-    arguments args >>>
-    Writer.tell [Machine (Printf.sprintf "call %s" name);
-                 Machine "push rax";]
-  | FuncDecl (_, name, local, args, body) ->
-    label name >>>
-    prolog >>>
-    move_arguments_to_stack local args >>>
-    assign_local_variable @@ Local.assign_size local >>>
-    let@ result = generate_nodes local body in
-    match result with
-    | Error _ as err -> Writer.return err
-    | Ok _ -> epiloge
+      List.fold_left
+        (fun pred (node, reg) ->
+           seq [pred;
+                generate_node local node;
+                tell [Machine "pop rax"];
+                tell [Machine (Printf.sprintf "mov %s, rax" reg)]])
+        (tell []) @@ List.rev maps in
+    seq [arguments args;
+         tell [Machine (Printf.sprintf "call %s" name);
+               Machine "push rax";]]
 
 and generate_nodes local nodes =
-  List.fold_left (fun pred node -> pred  >>> generate_node local node)
-    (Writer.return @@ (Ok ())) nodes
+  List.fold_left (fun pred node ->
+      seq [pred;
+           generate_node local node;])
+    (tell []) nodes
 
 let generate parsed =
-  let coms =
-    init >>>
-    let@ result = generate_nodes (Local.create ()) parsed in
-    match result with
-    | Error _ as err -> Writer.return err
-    | Ok _ -> Writer.return (Ok ())
-  in
-  Writer.run coms
+  let> state =
+    seq [init;
+         generate_nodes (Local.create ()) parsed;] in
+  Result.Monad.return @@ Writer.run state
   |> Result.map string_of_commands
--- writer.ml
+++ writer.ml
@@ -3,14 +3,12 @@ open Batteries
 type (+'a, 'b) t = ('a * 'b list)
 
 let return a = (a, [])
+
 let bind (a, s) f = let (b, t) = f a in (b, s @ t)
 
-let tell s =  (Ok (), s)
+let tell s =  ((), s)
 
-let run (result, b) =
-  match result with
-  | Ok _ -> Ok b
-  | Error err -> Error err
+let run (_, s) = s
 
 module Infix = struct
   let (let@) = bind
